a10905accb3ba70c4088f321842532f895ba80d9
Ward Cunningham <ward@c2.com>
2024-03-15 10:16:05 -0700

    add graphs in batches specified in markup

--- client/solo.js
+++ client/solo.js
@@ -9,31 +9,61 @@
       .replace(/\*(.+?)\*/g, '<i>$1</i>')
   }
 
+  // https://github.com/bitinn/node-fetch/issues/481
+  function absolute(url) {
+    return url.replace(/^(https?:)\/([^\/])/,`$1//${location.host}/$2`)
+  }
+
+  function parse(text) {
+    let graphs = []
+    let output = text.split(/\r?\n/).map (line => {
+      var m
+      if (m = line.match(/^ASSETS$/)) {
+        graphs.push(...assets())
+      } else if (m = line.match(/^LINK (\w+) (https?:\S+.jsonl)$/)) {
+        const graph = link(absolute(m[2]))
+        graphs.push(graph)
+        line = `LINK <a href="${absolute(m[2])}" target=_blank>${m[1]} <img src="/images/external-link-ltr-icon.png"></a>`
+      } else {
+        line = `<font color=gray>${expand(line)}</font>`
+      }
+      return line
+    }).join('<br>')
+    return {output, graphs}
+  }
+
+  async function assets() {
+    const schedule = []
+    return schedule
+  }
+
+  async function link(url) {
+    const text = await fetch(url).then(res => res.text())
+    const lines = text.trim().split(/\n/)
+    return lines.map(line => JSON.parse(line))
+  }
+
+
+  let parsed
   let todo = []
   async function emit($item, item) {
-    const testurl = 'https://raw.githubusercontent.com/WardCunningham/search/master/README.graph.jsonl'
-    const testjsonl = await fetch(testurl).then(res => res.text())
-    todo = testjsonl.trim().split(/\n/)
-      .map(line => JSON.parse(line))
-    const names = todo
-      .map(obj => obj.name)
-    return $item.append(`
-      <p style="background-color:#eee;padding:15px;">
-        ${names.join("<br>")}
-      </p><p>
-        <button onclick="window.plugins.solo.dopopup(event)">view in solo</button>
-      </p>`)
+    parsed = parse(item.text)
+    $item.append(`<p style="background-color:#eee;padding:15px;">
+      ${parsed.output}</p>`)
+    todo = await Promise.all(parsed.graphs)
+    $item.append(`<p>
+      <button onclick="window.plugins.solo.dopopup(event)">view in solo</button></p>`)
   }
 
   const dopopup = event => {
-    console.log('target',event.target)
+    const doing = {type:'batch', graphs:todo.shift()}
     const popup = window.open('/plugins/solo/dialog/#','solo','popup,height=720,width=1280')
     if (popup.location.pathname != '/plugins/solo/dialog/'){
       popup.addEventListener('load', event =>
-        popup.postMessage(todo.shift()))
+        popup.postMessage(doing))
     }
     else {
-      popup.postMessage(todo.shift(), window.origin)
+      popup.postMessage(doing, window.origin)
     }
   }
 
